e509bd43a0cefa08be080677d1f0cd0b15c1a40f
TheSmoothWater <thesmoothwater@gmail.com>
2025-03-09 21:50:43 -0400

    Works on my end

--- client.py
+++ client.py
@@ -21,9 +21,13 @@ def send_file(filename: str, address: (str, int)):
     ret_value = 0
     # get the file size in bytes
     # TODO: section 2 step 2
+    # Might not work as this is given code
+    file_size = get_file_size(filename)
+    print(f"File size: {file_size}")
 
     # convert file_size to an 8-byte byte string using big endian
     # TODO: section 2 step 3
+    byte_size_object = file_size.to_bytes(8, byteorder='big')
 
     # create a TCP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,15 +36,34 @@ def send_file(filename: str, address: (str, int)):
         # TODO: section 2 step 5
         # send the file size in the first 8-bytes followed by the bytes
         # for the file name to server at (IP, PORT)
+        client_socket.connect(address)
+        print("We've connected")
+
         # TODO: section 2 step 6
+        sendSizeName = f"{byte_size_object},{filename}".encode()
+        client_socket.sendto(sendSizeName, (IP, PORT))
+        print("We've sent the file info")
+
         # TODO: section 2 step 7
+        goAheadMessage = client_socket.recv(BUFFER_SIZE).decode()
+
+        if goAheadMessage != "go ahead":
+            raise OSError("Bad server response - was not go ahead!")
+
         # open the file to be transferred
-        with open(file_name, 'rb') as file:
+        with open(filename, 'rb') as file:
             # read the file in chunks and send each chunk to the server
             is_done = False
+            chunkSize = BUFFER_SIZE
             while not is_done:
                 # TODO: section 2 step 8a
+                chunk = file.read(chunkSize)
+                client_socket.sendto(chunk, (IP, PORT))
                 # TODO: section 2 step 8b
+                if not chunk:
+                    print("We broke out")
+                    is_done = True
+
     except OSError as e:
         ret_value = 2
         print(f'An error occurred while sending the file:\n\t{e}')
@@ -67,6 +90,8 @@ if __name__ == "__main__":
 
     ret_value = send_file(file_name, (IP, PORT))
     sys.exit(ret_value)
+
+    #send_file("TCPFileTransferProtocol.png", (IP, PORT))
 
 
 
--- server.py
+++ server.py
@@ -6,9 +6,18 @@ IP = '127.0.0.1'  # default IP address of the server
 PORT = 12000  # change to a desired port number
 BUFFER_SIZE = 1024  # change to a desired buffer size
 
+#Doesn't work as expected
+def get_file_info(data: str) -> (str, int):
+    seperator = data.find(",")
+    print(f"Data: {data}")
+    file_size = bytes.fromhex(data[2:seperator - 1].replace("\\x", ""))
+    file_size = int.from_bytes(file_size, byteorder="big")
+    print(f"Method File Size: {file_size}")
 
-def get_file_info(data: bytes) -> (str, int):
-    return data[8:].decode(), int.from_bytes(data[:8],byteorder='big')
+    file_name = data[seperator + 1:]
+    print(f"File name: {file_name}")
+
+    return file_name, file_size
 
 
 def upload_file(conn_socket: socket, file_name: str, file_size: int):
@@ -17,11 +26,17 @@ def upload_file(conn_socket: socket, file_name: str, file_size: int):
     # please do not change the above line!
     with open(file_name, 'wb') as file:
         retrieved_size = 0
+        print(f"File Size: {file_size}")
         try:
             while retrieved_size < file_size:
                 # TODO: section 1 step 6a
+                data = conn_socket.recv(BUFFER_SIZE)
                 # TODO: section 1 stop 6b
+                retrieved_size += len(data)
                 # TODO: section 1 stop 6c
+                file.write(data)
+                # print(f"Retrieved Size: {retrieved_size}")
+
         except OSError as oe:
             print(oe)
             os.remove(file_name)
@@ -35,12 +50,20 @@ def start_server(ip, port):
 
     try:
         while True:
-            (conn_socket, addr) = server_socket.accept()
+            conn_socket, addr = server_socket.accept()
             # TODO: section 1 step 2
             # expecting an 8-byte byte string for file size followed by file name
+            recvBytes, address = conn_socket.recvfrom(BUFFER_SIZE)
+            print("We received File Name")
             # TODO: section 1 step 3
+
+            file_info = recvBytes.decode()
+
+            file_name, file_size = get_file_info(file_info)
+
             print(f'Received: {file_name} with size = {file_size}')
             # TODO: section 1 step 4
+            conn_socket.sendto(b'go ahead', addr)
             upload_file(conn_socket, file_name, file_size)
             conn_socket.close()
     except KeyboardInterrupt as ki:
